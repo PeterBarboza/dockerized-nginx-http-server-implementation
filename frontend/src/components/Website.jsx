c192935fb00783e8029c4eca54f7c02a825156f7
PeterBarboza <pedrom.barboza01@gmail.com>
2024-03-06 01:16:10 -0300

    feat: Removing unused files

---
+++ frontend/src/components/Website.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Website.css'
+
+export function Website() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <button className="website-counter" onClick={() => setCount(prevState => prevState + 1)}>
+        Contagem: {count}
+      </button>
+    </div>
+  )
+}
